e6aa5643b7970f7d97da3f9301e6155de392ad96
Sergei Dmitriev <iserdmi@gmail.com>
2024-05-16 23:27:29 +0400

    Small fix

--- src/index.ts
+++ src/index.ts
@@ -17,21 +17,28 @@ export const applyDotEnvFiles = ({
   cwd,
   lastPathPart = '.env',
   withNodeEnvSuffig = true,
+  withProcessEnv = true,
 }: {
   cwd: string
   lastPathPart?: string
   withNodeEnvSuffig?: boolean
+  withProcessEnv?: boolean
 }) => {
+  const data = withProcessEnv ? { ...process?.env } : {}
   const envFilePath = findFilePath({ cwd, lastPathPart })
   if (envFilePath) {
     dotenv.config({ path: envFilePath, override: true })
+    Object.assign(data, dotenv.parse(fs.readFileSync(envFilePath)))
   }
   if (withNodeEnvSuffig) {
     const envFilePathWithNodeEnvSuffix = findFilePath({ cwd, lastPathPart: `${lastPathPart}.${process.env.NODE_ENV}` })
     if (envFilePathWithNodeEnvSuffix) {
       dotenv.config({ path: envFilePathWithNodeEnvSuffix, override: true })
+      Object.assign(data, dotenv.parse(fs.readFileSync(envFilePathWithNodeEnvSuffix)))
     }
   }
+  Object.assign(process.env, data)
+  return data
 }
 
 export const getDotEnvData = ({
