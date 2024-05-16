1fc5dbaf0f106b4ab4ba404afcc5bd4ceff99273
Sergei Dmitriev <iserdmi@gmail.com>
2024-05-16 18:32:11 +0400

    Small fix

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
+const findFilePath = ({ cwd, lastPathPart }: { cwd: string; lastPathPart: string }): string | null => {
+  const maybeEnvFilePath = path.join(cwd, lastPathPart)
+  if (fs.existsSync(maybeEnvFilePath)) {
+    return maybeEnvFilePath
+  }
+  if (cwd === '/') {
+    return null
+  }
+  return findFilePath({ cwd: path.dirname(cwd), lastPathPart })
+}
+
+export const applyDotEnvFiles = ({
+  cwd,
+  lastPathPart = '.env',
+  withNodeEnvSuffig = true,
+}: {
+  cwd: string
+  lastPathPart?: string
+  withNodeEnvSuffig?: boolean
+}) => {
+  const envFilePath = findFilePath({ cwd, lastPathPart })
+  if (envFilePath) {
+    dotenv.config({ path: envFilePath, override: true })
+  }
+  if (withNodeEnvSuffig) {
+    const envFilePathWithNodeEnvSuffix = findFilePath({ cwd, lastPathPart: `${lastPathPart}.${process.env.NODE_ENV}` })
+    if (envFilePathWithNodeEnvSuffix) {
+      dotenv.config({ path: envFilePathWithNodeEnvSuffix, override: true })
+    }
+  }
+}
+
+export const getDotEnvData = ({
+  cwd,
+  lastPathPart = '.env',
+  withNodeEnvSuffig = true,
+  withProcessEnv = true,
+}: {
+  cwd: string
+  lastPathPart?: string
+  withNodeEnvSuffig?: boolean
+  withProcessEnv?: boolean
+}) => {
+  const data = withProcessEnv ? { ...process?.env } : {}
+  const envFilePath = findFilePath({ cwd, lastPathPart })
+  if (envFilePath) {
+    Object.assign(data, dotenv.parse(fs.readFileSync(envFilePath)))
+  }
+  if (withNodeEnvSuffig) {
+    const envFilePathWithNodeEnvSuffix = findFilePath({ cwd, lastPathPart: `${lastPathPart}.${process.env.NODE_ENV}` })
+    if (envFilePathWithNodeEnvSuffix) {
+      Object.assign(data, dotenv.parse(fs.readFileSync(envFilePathWithNodeEnvSuffix)))
+    }
+  }
+  return data
+}
